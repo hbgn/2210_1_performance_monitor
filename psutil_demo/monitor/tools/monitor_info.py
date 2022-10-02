3bd96f82191689f359bce561a342b553b11d71e7
77kh <aikuny@126.com>
2022-10-02 22:05:10 +0800

    添加获取网卡信息方法

--- psutil_demo/monitor/tools/monitor_info.py
+++ psutil_demo/monitor/tools/monitor_info.py
@@ -1,6 +1,7 @@
-# from time import sleep
+from time import sleep
 import psutil
 import datetime
+import pprint
 
 
 class Monitor:
@@ -19,11 +20,42 @@ class Monitor:
     def mem(self):
         pass
 
+    # 获取网卡信息(适配器名称，ipv4，收发数据流)
+    def net(self):
+        address = psutil.net_if_addrs()  # ip
+        address_info = {
+            k: [
+                dict(
+                    family=data.family.name,
+                    addr=data.address,
+                    netmask=data.netmask,
+                    broadcast=data.broadcast,
+                )
+                for data in v if data.family.name == 'AF_INET'
+            ][0]
+            for k, v in address.items()
+        }
+        io = psutil.net_io_counters(pernic=True)
+        rs_data = [
+            dict(
+                name=k,
+                bytes_sent=v.bytes_sent,
+                bytes_recv=v.bytes_recv,
+                packets_sent=v.packets_sent,
+                packets_recv=v.packets_recv,
+                **address_info[k]
+            )
+            for k, v in io.items()
+        ]
+        return rs_data
+
     def dt(self):
         return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# if __name__ == '__main__':
-#     m = Monitor()
-#     for v in range(20):
-#         sleep(1)
-#         print(m.cpu())
+
+if __name__ == '__main__':
+    m = Monitor()
+    for v in range(1):
+        sleep(1)
+        # print(m.net())
+        pprint.pprint(m.net())
